6b98e7f8473201a724964b929dd2e727652cbe90
SushmaJettiboyina <sushma.jettiboyina@gmail.com>
2026-08-07 10:15:43 +0530

    Updated NumPy print statement

--- check_setup.py
+++ check_setup.py
@@ -5,6 +5,6 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 print("Python Version :", sys.version.split()[0])
-print("numpy :", np.__version__ )
+print("NumPy Version :", np.__version__ )
 print("pandas :", pd.__version__)
 print("matplotlib:", matplotlib.__version__)
